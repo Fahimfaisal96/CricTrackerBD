2758edcf1aa3c436eae76dd458db341f52a2cf6a
Fahim <fahimfaisal96.kpc@gmail.com>
2019-05-02 03:22:07 +0600

    Player Done final2

--- app/src/main/java/com/abc/crictrackerbd/showScorecard.java
+++ app/src/main/java/com/abc/crictrackerbd/showScorecard.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class showScorecard extends Activity {
+    String nothing;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
